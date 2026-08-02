e3f692a4cb2ddfe12051076696c37ba94de7538d
UmairAnsari17 <155252236+UmairAnsari17@users.noreply.github.com>
2026-08-02 19:49:38 +0530

    update project and resume

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ const projects = [
     title: "CineVault - Live",
     year: "2026",
     image: "/assets/projectCover/cinevault-img.png",
-    tech: "Python • Flask • Javascript • SQLAlchemy • PostgreSQL • Bootstrap",
+    tech: "Python • Flask • Javascript • SQLAlchemy • PostgreSQL • Bootstrap • CSS",
     description:
       "A personal movie tracker where users can add, edit, rate and review their favourite movies. Users can also rank movies as per their ratings.",
     github: "https://github.com/UmairAnsari17/CineVault",
@@ -17,7 +17,7 @@ const projects = [
     title: "Health Prediction System - Live Soon!",
     year: "2025-26",
     image: "/assets/projectCover/hps-img.png",
-    tech: "Python • Flask • Javascript • Machine Learning • SQL",
+    tech: "Python • Flask • Javascript • Machine Learning • SQLite • SQLAlchemy",
     description:
       "Predicts diseases based on symptoms using Machine Learning and provides health recommendations.",
     github: "https://github.com/UmairAnsari17/Health-prediction-system",
@@ -26,13 +26,13 @@ const projects = [
 
   {
     id: 3,
-    title: "Blog Web. - Live Soon!",
-    year: "2025",
-    image: "/assets/projectCover/blog-web-img.png",
-    tech: "Python • Flask • Javascript • SQLAlchemy • Bootstrap ",
+    title: "Restaurant Intelligence - Live Soon!",
+    year: "2026",
+    image: "/assets/projectCover/ri-img.png",
+    tech: "Python • Javascript • React+Vite • Node.js • FastAPI ",
     description:
       "A fully functional, multi-user blogging platform where an admin manages content and authenticated users can leave comments.",
-    github: "https://github.com/UmairAnsari17/blog-website",
+    github: "https://github.com/UmairAnsari17/restaurant-intelligence",
     live: "",
   },
 
